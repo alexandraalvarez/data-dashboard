e7f281625c2ae88da9242ab6cde6faca54dbd1f0
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2017-11-17 01:04:18 -0300

    Eliminé imágenes que no usaremos, adjunté carpeta fontawesome y modifiqué archivos HTML, CSS y JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-/*
- * Funcionalidad de tu producto
- */
+/*<i class="fa fa-times" aria-hidden="true"></i> para el fantasma
+
+En los menus cascada, falta agregar opciones de ver el overview, alumnos y profesores, cosa de que cuando quiera verlos, 
+pinche eso y listo */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
